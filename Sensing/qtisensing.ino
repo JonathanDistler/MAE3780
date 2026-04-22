eb87622304206533ef48416694cffab1f9969817
Lucas Libshutz <lsl94@cornell.edu>
2026-04-21 22:46:20 -0400

    starting comments
    need to clean up to actually use version control

--- Sensing/qtisensing.ino
+++ Sensing/qtisensing.ino
@@ -2,17 +2,17 @@
 #include <util/delay.h>
 #include <math.h>
 
+// Test script for sensing black color, only outputs message.
 // ---------------- MAIN SEQUENCE ----------------
 int main(void){
+  // Maps PC0 and PC1 to inputs, which correspond to A0 and A1 on the Uno.
   DDRC=0b11111100; //all are outputs except last two bits
   int num_count=0; 
-
 
     while(1){
       if (PINC & 0b00000010 || PINC & 0b00000001){ //if either is hi (sees black)
         Serial.println("see black!");
-        num_count=num_count+1;
-        
+        num_count++;
       }
       if (num_count>2){
         Serial.println("Too high"); 
--- Sensing/two_qtisensing_turn.ino
+++ Sensing/two_qtisensing_turn.ino
@@ -1,4 +1,5 @@
 //gives directions on how to turn, can implement the turn functions previously
+//NOTE: need to change to binary pin numbers and not PC0 and PC1, will get points off
 void setup() {
   Serial.begin(9600);
   DDRC &= ~((1 << PC0) | (1 << PC1));
@@ -8,6 +9,7 @@ void loop() {
   while (1) {
     uint8_t pins = PINC & ((1 << PC0) | (1 << PC1));
 
+    //logic to find which side of the car the black color is on.
     if (!(pins & (1 << PC0))) {
       Serial.println("turn right");   // A0 sees black
     }
